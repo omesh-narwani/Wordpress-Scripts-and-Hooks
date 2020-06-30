008c1a0c24fdc4c4083226d28d826645ef633b90
omesh kumar <omesh.narwani40@gmail.com>
2020-06-30 14:06:25 +0500

    Add files via upload

---
+++ wordpress_functions_page_hooks/list-categories.php
@@ -0,0 +1,25 @@
+ // List of Categories
+ <ul>
+	<?php
+	  $taxonomy     = 'product_cat';
+	  $orderby      = 'name';  
+	  $show_count   = 0;      // 1 for yes, 0 for no
+	  $pad_counts   = 0;      // 1 for yes, 0 for no
+	  $hierarchical = 1;      // 1 for yes, 0 for no  
+	  $title        = '';  
+	  $empty        = 0;
+
+	  $args = array(
+			 'taxonomy'     => $taxonomy,
+			 'orderby'      => $orderby,
+			 'show_count'   => $show_count,
+			 'pad_counts'   => $pad_counts,
+			 'hierarchical' => $hierarchical,
+			 'title_li'     => $title,
+			 'hide_empty'   => $empty
+	  );
+		$terms = get_categories($args);
+		foreach($terms as $cat){?>
+			<li><a href="#"><?= $cat->name; ?></a></li>
+		<?php }?>
+</ul>
